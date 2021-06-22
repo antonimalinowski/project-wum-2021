d0d2c4a99415757107272c7b22d280447a0b5228
antek <antonimalinowski.kontakt@gmail.com>
2021-06-22 20:38:44 +0200

    connected to firebase with imported json data and added config files

--- wum-2021/src/App.js
+++ wum-2021/src/App.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import Card from './Card/Card';
 import DrawButton from './DrawButton/DrawButton';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+import { DB_CONFIG } from './Config/Firebase/db_config.js';
 
 class App extends Component {
 constructor(props){
   super(props);
-
+  
+  if (!firebase.apps.length) {
+    firebase.initializeApp(DB_CONFIG);
+}
+  this.database = firebase.database().ref().child('cards');
   this.updateCard = this.updateCard.bind(this);
 
   this.state = {
-    cards: [
-      {id: 1, eng: "English", han: "Hanzi", pin: "Pinyin"},
-      {id: 2, eng: "English_2", han: "Hanzi_2", pin: "Pinyin_2"},
-    ],
+    cards: [],
     currentCard: {}
   }
 }
@@ -21,9 +26,17 @@ constructor(props){
 componentWillMount() {
   const currentCards = this.state.cards;
 
-  this.setState({
-    cards: currentCards,
-    currentCard: this.getRandomCard(currentCards)
+  this.database.on('child_added', snap => {
+    currentCards.push({
+      id: snap.key,
+      eng: snap.val().eng,
+      han: snap.val().han,
+      pin: snap.val().pin,
+    })
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards)
+    })
   })
 }
 

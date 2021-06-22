d5fa9b62e5eb6abb96f079dc9d6c132289ef4870
antek <antonimalinowski.kontakt@gmail.com>
2021-06-22 07:43:37 +0200

    Displaying single flashcard; random display on every refresh

--- wum-2021/src/App.js
+++ wum-2021/src/App.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+import Card from './Card/Card';
 
-function App() {
-  return (
-    <h1>Hello world</h1>
-  );
+class App extends Component {
+constructor(props){
+  super(props);
+
+  this.state = {
+    cards: [
+      {id: 1, eng: "English", han: "Hanzi", pin: "Pinyin"},
+      {id: 2, eng: "English_2", han: "Hanzi_2", pin: "Pinyin_2"},
+    ],
+    currentCard: {}
+  }
+}
+
+componentWillMount() {
+  const currentCards = this.state.cards;
+
+  this.setState({
+    cards: currentCards,
+    currentCard: this.getRandomCard(currentCards)
+  })
+}
+
+getRandomCard(currentCards) {
+  var card = currentCards[Math.floor(Math.random() * currentCards.length)];
+  return(card);
+}
+
+  render() {
+    return (
+      <div className="App">
+        <Card eng={this.state.currentCard.eng}
+              han={this.state.currentCard.han}
+              pin={this.state.currentCard.pin}
+              />
+      </div>
+    );
+  }
 }
 
 export default App;

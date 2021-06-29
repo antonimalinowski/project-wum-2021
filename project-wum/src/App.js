b0ce5712da04e252e8ffa364cafa7209521def31
antek <antonimalinowski.kontakt@gmail.com>
2021-06-29 06:41:06 +0200

    flashcard with in-file data - loads images and text, displays random card on refresh

--- project-wum/src/App.js
+++ project-wum/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
+import Fiszki from './Data/set1';
+import Card from './Card';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cards: [
+        {
+            question: 'Wyjaśnij pojęcie niedożywienie, uwzględniając najważniejsze informacje',
+            frontimage: '',
+            answer: 'Stan wynikający z braku wchłaniania lub braku spożywania substancji żywieniowych, prowadzący do zmiany składu ciała, upośledzenia fizycznej i mentalnej funkcji organizmu oraz wpływający niekorzystnie na wynik leczenia choroby podstawowej',
+            backimage: 'https://randomuser.me/api/portraits/men/75.jpg',
+        },
+        {
+            question: 'PRAWDA/ FAŁSZ: Chorym ze znaczną nadwagą lub otyłością najczęściej towarzyszy niedożywienie typu Kwashiorkor.',
+            frontimage: '',
+            answer: 'PRAWDA',
+            backimage: './Images/grafika 18.jpg',
+        },
+        {
+            question: 'Wymień 3 typy niedożywienia',
+            frontimage: '',
+            answer: 'Niedożywienie typu marasmus (niedożywienie białkowo-kaloryczne), \n Niedożywienie typu kwashiorkor (niedożywienie białkowe), \n Niedożywienie mieszane \n',
+            backimage: '',
+        },
+        {
+          question: 'Jaką znasz inną nazwę na niedożywienie typu marasmus?',
+          frontimage: '',
+          answer: 'Niedożywienie proste',
+          backimage: '',
+        },
+        {
+          question: 'Czy osoby niedożywione zawsze są wychudzone?',
+          frontimage: '',
+          answer: 'NIE',
+          backimage: '',
+        },
+      ],
+        currentCard: {}
+    };
+  }
+
+  state = {
+    loading: true,
+    person: null
+  };
+
+  componentWillMount() {
+    const currentCards = this.state.cards;
+    
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards),
+    })
+  }
+
+  getRandomCard(currentCards) {
+    var card = currentCards[Math.floor(Math.random() * currentCards.length)]
+    return(card);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Card
+          question={this.state.currentCard.question}
+          frontimage={this.state.currentCard.frontimage}
+          answer={this.state.currentCard.answer}
+          backimage={this.state.currentCard.backimage}/>
+      </div>
+    );
+  }
 }
 
 export default App;

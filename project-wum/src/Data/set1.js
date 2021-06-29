a503ff657a99be25e3796108a5778ab71f651290
antek <antonimalinowski.kontakt@gmail.com>
2021-06-29 06:37:46 +0200

    set1 data for possible future use

---
+++ project-wum/src/Data/set1.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+// class Data extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       fiszki: [
+//         {
+//                   question: 'Wyjaśnij pojęcie niedożywienie, uwzględniając najważniejsze informacje',
+//                   frontimage: '',
+//                   answer: 'Stan wynikający z braku wchłaniania lub braku spożywania substancji żywieniowych, prowadzący do zmiany składu ciała, upośledzenia fizycznej i mentalnej funkcji organizmu oraz wpływający niekorzystnie na wynik leczenia choroby podstawowej',
+//                   backimage: 'https://randomuser.me/api/portraits/men/72.jpg',
+//         },
+//         {
+//                   question: 'PRAWDA/ FAŁSZ: Chorym ze znaczną nadwagą lub otyłością najczęściej towarzyszy niedożywienie typu Kwashiorkor.',
+//                   frontimage: 'https://randomuser.me/api/portraits/men/65.jpg',
+//                   answer: 'PRAWDA',
+//                   backimage: './Images/grafika 18.jpg',
+//         },
+//       ]
+//     };
+//   }
+// }
+
+const fiszki = [
+    {
+              question: 'Wyjaśnij pojęcie niedożywienie, uwzględniając najważniejsze informacje',
+              frontimage: '',
+              answer: 'Stan wynikający z braku wchłaniania lub braku spożywania substancji żywieniowych, prowadzący do zmiany składu ciała, upośledzenia fizycznej i mentalnej funkcji organizmu oraz wpływający niekorzystnie na wynik leczenia choroby podstawowej',
+              backimage: 'https://randomuser.me/api/portraits/men/72.jpg',
+    },
+    {
+              question: 'PRAWDA/ FAŁSZ: Chorym ze znaczną nadwagą lub otyłością najczęściej towarzyszy niedożywienie typu Kwashiorkor.',
+              frontimage: 'https://randomuser.me/api/portraits/men/65.jpg',
+              answer: 'PRAWDA',
+              backimage: './Images/grafika 18.jpg',
+    },
+  ]
+
+  export default fiszki
